0cc07d8d27ffc511d4a8f102813a886e609150c7
Patrick Schneider <ownonline@outlook.com>
2024-06-08 10:04:02 +0200

    Upload Release of Version 0.0.1.7 Gardenia (TESTRELEASE / PRERELASE)
    -	Language manager implemented. This already has an influence on the main interface and the sound extension. The overrides for the large error messages when the player crashes are still missing. Here, communication is still in German. The Baseaudio extension is currently not usable. It is currently missing important translation files, which is why it cannot be used properly at all. The user interface selects the displayed language based on the URL parameter “hl”. If this parameter is not found in the URL, an automatic attempt is made to use the language used by your browser. If we do not offer the user interface in this language, the page will be displayed in English. The languages currently available for the above-mentioned areas are German, English and basic Italian.
    -   Laut extension: The placeholder covers for Laut.fm are no longer in use. If no Laut.de cover can be loaded, the station logo is now shown instead. If the station is LIVE, the station logo is currently always displayed. The reason for this is that the information “Station is live” will soon be moved above instead of being displayed on the cover.
    -   Changed Browser Version Requirements: Internet Explorer 12<; Firefox 75<; Chrome 80<; Safari 13<; Opera 65<;
    -   Redesign of the "About oOPlay" dialog, also slight layout changes in other windows by the translation management. Windows of oOPlay direct are now positioned in the center of the screen. The windows of the extensions will follow.
    -   The BASEAUDIO extension has been redesigned and now matches the design of the new oOPlay homepage, which will be online shortly after this release.
    -   Further design changes in line with the new oOPlay homepage

---
+++ Releases/0-0-1-7/engine/ver.php
@@ -0,0 +1,36 @@
+<?php 
+$pro_name = 'oOPlay';
+$pro_version = '0.0.1.7';
+$pro_version_name = 'Gardenia';
+$pro_license = 'MIT-Lizenz';
+$pro_license_url = 'https://www.tldrlegal.com/license/mit-license';
+$pro_releasedate = '31.05.2024';
+$pro_company = 'ownonline/Patrick Schneider';
+$pro_company_url = 'https://ownonline.eu/';
+$pro_copyright = '2024';
+
+
+echo "<div class='container'><div class='row'><div class='col-8'>";
+ 
+
+echo "<h3 ><b>" . $pro_name . "</b> <i> ". $pro_version_name . "</i></h3><p class='lead'>" . $lang['about_brand_phrase'];
+
+echo "</div><div class='col-4'><img style ='margin: 0.2em;' class='rounded' src='rscs/imglibs/social/oop_orbs/oop-l-color.png' width='100%' height='auto' alt='oOPlay'></div></div></div>";
+
+echo "Version <kbd>" . $pro_version . "</kbd> " . $lang['from_who'] . " " . $pro_releasedate . " <br><small>" . 
+"Copyright " . $pro_copyright . " by <a target='_blank' href='" . $pro_company_url . "'>" . $pro_company ."</a> <br> " . $lang['about_license_owner_is'] . "<kbd><a href='" . $copyowner_url . "'>". $copyowner . " (" . $_SERVER['HTTP_HOST'] . ")</a></kbd><br><a target='_blank' href='" . $pro_license_url ."'>" . $pro_license . " " . $lang['about_license_datewording'] . " " . $pro_releasedate . ")</a> <hr>";
+
+    // Bedingung zum Anzeigen der Erweiterungsguthaben
+    if ($enable_extension_credits) {
+        echo $extensions_credits;
+        echo "</div></div>";
+    }
+
+
+ echo "<br><br><div class='alert bg-danger'>" . $lang['about_prerelease_warning_title'] . " " . $pro_version ."<hr>" . $lang['about_prerelease_warning_p1'] . " '". $pro_name. "' " . $lang['about_prerelease_warning_p2'] . "</div>"; 
+ 
+ 
+echo "<br>" . $lang['about_documentation_p1'] . " <a target='_blank' href='https://oop.ownonline.eu/?ver=" . $pro_version ."'>" . $lang['about_documentation_p2'] . "</a>" . $lang['about_documentation_p3'] . "</small>";
+
+    echo "<hr><small>" . $lang['about_legal_p1'] . " <a target='_blank' href='" . $privacy_url . "'>" . $lang['about_legal_p2'] . "</a> " . $lang['about_legal_p3'] . " <a target='_blank' href='" . $impress_url . "'>" . $lang['about_legal_p4'] . "</a> " . $lang['from'] . " " . $copyowner . "" . $lang['about_legal_p5'] . " " . $lang['about_legal_p6'] . " " . $lang['from'] . " " . $pro_name . " " . $lang['about_legal_p7'] . " <a href='https://oop.ownonline.eu/eula/?ver=" . $pro_version ."'>". $lang['about_legal_p8'] . "</a>". $lang['about_legal_p9'] . " </small>";
+?>
